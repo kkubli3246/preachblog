b1543044bb0ac1dafe2aa69688e6a1cee057ac0b
elale1984 <edward.gratefuldead1@gmail.com>
2022-07-14 12:52:47 -0500

    Added css stylesheets. Added Service layer. Added Security Service that mimics login

---
+++ src/main/java/com/gcu/preach/model/BlogPost.java
@@ -0,0 +1,14 @@
+package com.gcu.preach.model;
+
+import java.util.Date;
+
+public class BlogPost {
+
+    private int id;
+    private int pictureId;
+    private String title;
+    private String content;
+    private String author;
+    private Date date;
+    
+}
--- src/main/java/com/gcu/preach/model/LoginModel.java
+++ src/main/java/com/gcu/preach/model/LoginModel.java
@@ -1,4 +1,4 @@
-package com.gcu.model;
+package com.gcu.preach.model;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
